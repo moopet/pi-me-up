6147fb9d6a89880483f3a7931b8b1d7a54d8d608
Ben Sinclair <ben@moopet.net>
2012-11-20 16:39:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+pimeup
+======
+
+A quick deployment script for the Raspberry Pi.
+
+Requirements
+------------
+
+A working pi, with SSH installed and running (as it is by default)
+You'll need to know its IP address or hostname.
+
+Python 2.5+
+[Fabric](http://fabfile.org) and Cuisine
+
+
+Setup
+-----
+
+Download the fabfile or clone the repository.
+
+    $ pip install fabric cuisine
+    $ fab -l
+
+That's all.
+
+Usage
+-----
+
+To get a list of available commands:
+
+    fab -l
+
+At the moment these are
+* deploy               Installs pretty much everything to a bare Pi.
+* install\_firewall     Installs ufw and opens ssh access to everyone.
+* install\_motd         Installs a succulent ascii-art MOTD. In colour!
+* install\_mpd          Installs MPD and configures it for the 3.5mm audio output.
+* install\_my\_dotfiles  Copies down my dotfiles repository from GitHub.
+* install\_usb\_wifi     Configures a generic USB WiFi device for DHCP.
+* open\_port            Adds a firewall rule to allow EVERYONE access to the specified port.
+* setup\_packages       Installs basic Raspbian package requirements.
+* setup\_python         Installs virtualenvwrapper and some common global python packages.
+* status               General stats about the Pi.
+* update\_firmware      Updates firmware. See https://github.com/Hexxeh/rpi-update for more.
+* upgrade\_packages     Does a full apt-get upgrade.
+
+To specify your hostname, either edit the fabfile, use the _-H_ switch on the 
+command line, or fabric will prompt you when you run a command.
+
+Examples
+--------
+
+    $ fab -H 192.168.1.23 deploy
+    $ fab -H 192.168.1.23 open_port:port=8080
+    $ fab -H 192.168.1.23 status
+
+
+Notes
+-----
+
+This isn't particularly bright. It's not like chef, and it won't gracefully 
+update your current configuration, but will instead overwrite it with a 
+reckless and carefree abandon. At least, it will how I write it.
+Some things are easy to handle, such as adding virtualenv setup to the bash
+settings, but others really need human eyes to figure out what's already in the
+file.
+
+For example, if you have eth0 configured with a static IP address and run
+
+    fab install_usb_wifi
+
+Then you'll lose that setting. This isn't a full management suite.
+It's very scrappy, but that's because I haven't finished setting my Pi up how
+I want it - it's going to do double-duty as a WiFi radio and an IRC/bitlbee host
+so it's probably going to be geared towards that at the moment.
+
+License
+-------
+
+MIT. See LICENCE file.
---
+++ fabfile.py
@@ -0,0 +1,357 @@
+#!/usr/bin/env python
+# vim: set fileencoding=UTF-8 :
+
+from cuisine import file_exists, dir_exists, file_write, text_strip_margin
+from cuisine import package_ensure as _package_ensure
+from fabric.api import sudo, run, env, hide, cd, task
+from fabric.contrib.files import append
+from fabric.utils import puts
+from fabric.colors import red, green
+
+LOCAL_PREFIX = "/usr/local/bin"
+INDENT = "→ "
+
+env.user = "pi"
+#env.hosts = ["rpi"]
+
+
+def install_binary_from_URL(url):
+    """ Download a file from a URL and install it to the current prefix
+    directory. Make sure you trust the URL...
+    """
+    context = {
+        "p": LOCAL_PREFIX,
+        "u": url,
+        "f": url.split("/")[-1],
+    }
+    puts("Installing {u} to {p}/{f}".format(**context))
+    with hide("output", "running"):
+        if file_exists("{p}/{f}".format(**context)):
+            sudo("rm {p}/{f}".format(**context))
+        sudo("wget {u} -O {p}/{f}".format(**context))
+        sudo("chmod +x {p}/{f}".format(**context))
+
+
+def sudo_file_write(filename, contents):
+    """ (Over)write a file as root. This is a substitute for fabric"s
+    file_write for writing global configuration files.
+    """
+    with hide("output", "running"):
+        temp_file = run("mktemp")
+        file_write(temp_file, contents)
+        sudo("cp -r {s} {d}".format(s=temp_file, d=filename))
+        sudo("chmod 644 {}".format(filename))
+        sudo("rm {}".format(temp_file))
+
+
+def update_packages():
+    """ Clean-displaying wrapper for "apt-get update".
+    Doesn"t run more than once per fabric session.
+    """
+    if not hasattr(update_packages, "done"):
+        puts(green("{} updating packages".format(INDENT)))
+        with hide("output", "running"):
+            sudo("apt-get update")
+        update_packages.done = True
+
+
+def package_ensure(package):
+    """ Prettier-printing version of cuisine's package_ensure.
+    Doesn't bother to run if it's already been called for this package in
+    this fabric session.
+    """
+    if not hasattr(package_ensure, "checked"):
+        package_ensure.checked = []
+    if package not in package_ensure.checked:
+        with hide("running", "output"):
+            puts("{i} checking {p}".format(i=INDENT, p=package))
+            _package_ensure(package)
+            package_ensure.checked.append(package)
+
+
+def global_pip_install(package):
+    """ Pretty-printing, internal wrapper for 'sudo pip install'.
+    Doesn't bother to run if it's already been called for this package in
+    this fabric session.
+    """
+    if not hasattr(global_pip_install, "checked"):
+        global_pip_install.checked = []
+    if package not in global_pip_install.checked:
+        with hide("running", "output"):
+            package_ensure("python-pip")
+            puts("{i} checking {p}".format(i=INDENT, p=package))
+            sudo("pip install {}".format(package))
+            global_pip_install.checked.append(package)
+
+
+@task
+def install_my_dotfiles():
+    """ Copies down my dotfiles repository from GitHub.
+    Installs only those files which might be relevant to the Raspberry Pi.
+    See http://github.com/moopet/dotfiles
+    """
+    puts(green("Installing dotfiles"))
+    dotfiles = (
+        ".vimrc",
+        ".ackrc",
+        ".htoprc",
+        ".gitignore",
+        ".gitconfig",
+        ".fonts",  # patched font for vim-powerline
+        ".tmux.conf",
+    )
+    with hide("output", "running"), cd("/tmp"):
+        if dir_exists("dotfiles"):
+            with cd("dotfiles"):
+                run("git pull")
+        else:
+            run("git clone git://github.com/moopet/dotfiles.git")
+        for f in dotfiles:
+            puts("{i} {f}".format(i=INDENT, f=f))
+            run("cp -r dotfiles/{} ~/".format(f))
+
+
+@task
+def install_usb_wifi(ssid, psk):
+    """ Configures a generic USB WiFi device for DHCP.
+    This overwrites /etc/network/interfaces, so any changes you have made
+    will be lost;  eth0 is reset to DHCP.
+    usage: install_usb_wifi:ssid=<MY_SSID>,psk=<MY_PSK>
+    """
+
+    puts(green("Installing USB WiFi device"))
+    wpa_conf = text_strip_margin("""
+        |network={{
+        |    ssid="{ssid}"
+        |    proto=RSN
+        |    key_mgmt=WPA-PSK
+        |    pairwise=CCMP TKIP
+        |    group=CCMP TKIP
+        |    psk="{psk}"
+        |}}
+    """.format(ssid=ssid, psk=psk))
+
+    interfaces = text_strip_margin("""
+        |auto lo
+
+        |iface lo inet loopback
+        |iface eth0 inet dhcp
+
+        |auto wlan0
+        |iface wlan0 inet dhcp
+        |wpa-conf /etc/wpa.conf
+    """)
+
+    sudo("ifdown --force wlan0; true")
+    sudo_file_write("/etc/network/interfaces", interfaces)
+    sudo_file_write("/etc/wpa.conf", wpa_conf)
+    sudo("ifup wlan0")
+
+
+@task
+def install_motd():
+    """ Installs a succulent ascii-art MOTD. In colour!
+    The raspberry was by RPi forum user b3n, taken from
+    http://www.raspberrypi.org/phpBB3/viewtopic.php?f=2&t=5494
+    """
+
+    puts(green("Installing succulent MOTD"))
+    motd = text_strip_margin("""
+        |
+        |{g}      .~~.   .~~.
+        |{g}     ". \ " " / ."
+        |{r}      .~ .~~~..~.
+        |{r}     : .~."~".~. :    {b}                       __                      {o}     _
+        |{r}    ~ (   ) (   ) ~   {b}    _______ ____ ___  / /  ___ __________ __  {o}___  (_)
+        |{r}   ( : "~".~."~" : )  {b}   / __/ _ `(_-</ _ \/ _ \/ -_) __/ __/ // / {o}/ _ \/ /
+        |{r}    ~ .~ (   ) ~. ~   {p}  /_/  \_,_/___/ .__/_.__/\__/_/ /_/  \_, / {o}/ .__/_/
+        |{r}     (  : "~" :  )    {p}              /_/                    /___/ {o}/_/
+        |{r}      "~ .~~~. ~"
+        |{r}          "~"
+        |{n}
+        |
+        |""".format(
+            g="[32m",
+            r="[31m",
+            b="[34m",
+            o="[33m",
+            p="[35m",
+            n="[m",
+        )
+    )
+    with hide("output", "running"):
+        sudo_file_write("/etc/motd", motd)
+
+
+@task
+def setup_packages():
+    """ Installs basic Raspbian package requirements.
+    """
+    puts(green("Installing packages"))
+    update_packages()
+
+    with hide("running"):
+        package_ensure("git-core")
+        package_ensure("mpc")
+        package_ensure("mpd")
+        # Sometimes I use screen, sometimes I use tmux ...
+        package_ensure("screen")
+        package_ensure("tmux")
+        # ... but I always use vim.
+        package_ensure("vim")
+        package_ensure("python-pip")
+
+        package_ensure("ack-grep")
+        ack_filename = "{}/ack".format(LOCAL_PREFIX)
+        if file_exists(ack_filename):
+            sudo("rm {}".format(ack_filename))
+        sudo("ln -s /usr/bin/ack-grep {}".format(ack_filename))
+
+    install_binary_from_URL(
+        "https://raw.github.com/sjl/friendly-find/master/ffind"
+    )
+
+
+@task
+def setup_python():
+    """ Installs virtualenvwrapper and some common global python packages.
+    """
+    puts(green("Setting up global python environment"))
+    global_pip_install("ipython")
+    global_pip_install("ipdb")
+    global_pip_install("virtualenv")
+    global_pip_install("virtualenvwrapper")
+    puts("adding virtualenvwrapper to .bashrc".format(INDENT))
+    with hide("everything"):
+        append(".bashrc", "export WORKON_HOME=~/.virtualenvs")
+        append(".bashrc", ". $(which virtualenvwrapper.sh)")
+
+
+@task
+def update_firmware():
+    """ Updates firmware. See https://github.com/Hexxeh/rpi-update for more
+    information.
+    """
+    update_packages()
+    puts(red("Updating firmware"))
+    with hide("output", "running"):
+        package_ensure("ca-certificates")
+        sudo("wget http://goo.gl/1BOfJ -O /usr/bin/rpi-update")
+        sudo("chmod +x /usr/bin/rpi-update")
+    sudo("rpi-update")
+
+
+@task
+def install_firewall():
+    """ Installs ufw and opens ssh access to everyone.
+    """
+    puts(green("Installing/configuring firewall"))
+    with hide("output", "running"):
+        package_ensure("ufw")
+        sudo("ufw allow proto tcp from any to any port 22")
+        sudo("ufw --force enable")
+
+
+@task
+def open_port(port):
+    """ Adds a firewall rule to allow EVERYONE access to the specified port.
+    """
+    puts(green("Configuring firewall to allow all on port {}".format(port)))
+    with hide("output", "running"):
+        install_firewall()
+        sudo("ufw allow proto tcp from any to any port {}".format(port))
+        sudo("ufw --force enable")
+
+
+@task
+def install_mpd():
+    """ Installs MPD and configures it for the 3.5mm audio output.
+    Allows passwordless connection from any host on port 6600.
+    """
+    package_ensure("mpc")
+    package_ensure("mpd")
+    package_ensure("ufw")
+    with hide("output", "running"):
+        sudo("ufw allow proto tcp from any to any port 6600")
+        sudo("ufw --force enable")
+        mpd = text_strip_margin("""
+            |music_directory         "/var/lib/mpd/music"
+            |playlist_directory      "/var/lib/mpd/playlists"
+            |db_file                 "/var/lib/mpd/tag_cache"
+            |log_file                "/var/log/mpd/mpd.log"
+            |pid_file                "/var/run/mpd/pid"
+            |state_file              "/var/lib/mpd/state"
+            |sticker_file            "/var/lib/mpd/sticker.sql"
+            |user                    "mpd"
+            |port                    "6600"
+            |filesystem_charset      "UTF-8"
+            |id3v1_encoding          "UTF-8"
+            |follow_outside_symlinks "yes"
+            |follow_inside_symlinks  "yes"
+            |zeroconf_enabled        "yes"
+            |zeroconf_name           "Raspberry Pi"
+            |volume_normalization    "yes"
+            |max_connections         "10"
+            |input {
+            |    plugin              "curl"
+            |}
+            |audio_output {
+            |    type                "alsa"
+            |    name                "3.5mm Headphone Jack"
+            |    device              "hw:0,0"
+            |    format              "44100:16:2"
+            |    mixer_device        "default"
+            |    mixer_control       "PCM"
+            |    mixer_index         "0"
+            |}
+            |audio_output {
+            |    type                "alsa"
+            |    name                "USB Audio"
+            |    device              "hw:0,0"
+            |    format              "44100:16:2"
+            |    mixer_device        "software"
+            |    mixer_control       "PCM"
+            |    mixer_index         "1"
+            |}
+            |mixer_type              "software"
+        """)
+
+    sudo_file_write("/etc/mpd.conf", mpd)
+    sudo("/etc/init.d/mpd restart")
+
+
+@task
+def upgrade_packages(force=None):
+    """ Does a full apt-get upgrade.
+    If force is set to true, it will not prompt for confirmation.
+    """
+    update_packages()
+    if force:
+        puts(red("Upgrading all packages"))
+        with hide("output", "running"):
+            sudo("apt-get upgrade --assume-yes")
+    else:
+        puts(green("Upgrading packages"))
+        sudo("apt-get upgrade")
+
+
+@task
+def status():
+    """ General stats about the Pi. """
+    with hide("running", "stderr"):
+        run("mpc")
+        run("uptime")
+        run("df -h")
+
+
+@task
+def deploy():
+    """ Installs pretty much everything to a bare Pi.
+    """
+    puts(green("Starting deployment"))
+    setup_packages()
+    setup_python()
+    install_firewall()
+    install_my_dotfiles()
+    install_mpd
+    install_motd()
